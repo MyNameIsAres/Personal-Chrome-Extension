76b952c49a070ae29a47f9719d6c86c5e10a5abc
MyNameIsAres <chrisvonk@hotmail.nl>
2020-07-23 20:27:01 +0200

    Minor changes.

--- public/weather.js
+++ public/weather.js
@@ -7,15 +7,56 @@
 const weather = document.getElementById('weather');
 
 // Obviously hidden, duh..
-const api = 'lol, you wish';
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=52.2628208&lon=4.4589903&appid=' + api;
+const api = '';
 
-
-function getWeatherData() {
-    fetch(url)
-        .then(data => data.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.log(error));
+function convertKelvinToCelcius(kelvinTemp) {
+    return Math.floor(kelvinTemp - 273.15);
 }
 
-getWeatherData();
+
+
+let getPosition = function () {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+
+getPosition()
+    .then((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(latitude + ' ' + longitude);
+        getWeatherData(latitude, longitude);
+    })
+    .catch((error) => {
+        console.log("Error has occured " + error);
+    });
+
+
+function getWeatherData(latitude, longitude) {
+    console.log(latitude);
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api}`;
+    fetch(url)
+        .then(data => data.json())
+        .then((data) => {
+            console.log(data);
+            let celcius = convertKelvinToCelcius(data.main.temp);
+            let testIcon = data.weather.icon;
+            document.getElementById("weather").innerHTML = icon;
+            console.log(celcius);
+          
+        })
+                .catch((error) => console.log(error));
+}
+
+
+// Will fix tomorrow
+// getPosition()
+//     .then((position) => {
+//         const {latitude, longitude} = position.coords
+//         console.log(latitude + ' ' + longitude);
+//         getWeatherData(latitude, longitude);
+//     })
+//     .catch((error) => {
+//         console.log("Error has occured " + error);
+//     });
