3df5632fafdb138a1854ef844efd28ce686fd2e5
MyNameIsAres <chrisvonk@hotmail.nl>
2020-07-24 20:35:29 +0200

    Included custom search bar that directs all search to Exosia, a search engine that will plant a tree for every 45 search results you make!

--- public/newtab.js
+++ public/newtab.js
@@ -13,14 +13,23 @@ function greeting() {
     const time = new Date().getHours();
     const name = "Ares";
 
-    greeting.innerHTML = `${(hour >= 18) ? `Good evening` : 
-    (hour <18 && hour >= 12) ? `Good afternoon` :
-    (hour < 12) ? `Good Morning` : '' }, `;
+    greeting.innerHTML = `${(hour >= 18) ? `Good evening` :
+        (hour < 18 && hour >= 12) ? `Good afternoon` :
+            (hour < 12) ? `Good Morning` : ''}, `;
 
     greeting.prepend(name);
 
-    
 
+
+}
+
+document.getElementById('form', addEventListener('submit', search));
+
+function search(e) {
+    e.preventDefault();
+    let text = document.getElementById('text').value;
+    let x =   window.location = "https://www.ecosia.org/search?q=" + encodeURIComponent(text);
+    console.log(x);
 }
 
 // Rename eventually
